8df35f064609fea072f16250b07f0cca335ea5d8
Nguyễn Xuân Huỳnh <xuanhuynh543@gmail.com>
2025-07-21 08:48:33 +0700

    first commit

---
+++ src/main/java/com/webservice/setupjava/SetupJavaApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.setupjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetupJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webservice/setupjava/SetupJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.setupjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetupJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
